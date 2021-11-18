bb5938f90ae64c42caf5a083359e678dd38f48a6
Chen <72628138+cangtiandi@users.noreply.github.com>
2021-11-18 11:10:01 -0600

    Added some things to proposal and added blocks in sketch

--- proposal.md
+++ proposal.md
@@ -2,7 +2,10 @@
 
 ## Must Have List
 - The game Tetris 
+- Score counter 
+- Ghost piece that shows you where to piece is gonna drop
 
 ## Nice to Have List
 - The color changes when the score reaches a certain amount
 - The speed changes when the score reaches a certain amount
+- Hard drop
--- sketch.js
+++ sketch.js
@@ -1,20 +1,85 @@
+let grid;
+let gridWidth = 10;
+
+let types = {
+  block: [
+    ["red", "red", null],
+    ["red", "red", null],
+    [null, null, null],
+  ],
+  lineBlock: [
+    [null, null, null, null],
+    ["red", "red", "red", "red"],
+    [null, null, null, null]
+  ],
+  tBlock: [
+    [null, "red", null],
+    ["red", "red", "red"],
+    [null, null, null]
+  ],
+  lblock: [
+    ["red", null, null],
+    ["red","red","red"],
+    [null, null, null]
+  ],
+  reverseLBlock: [
+    [null, null, "red"],
+    ["red","red","red"],
+    [null, null, null]
+  ],
+  zBlock: [
+    ["red", "red", null],
+    [null, "red", "red"],
+    [null, null, null]
+  ],
+  reverseZBlock: [
+    [null, "red", "red"],
+    ["red", "red", null],
+    [null, null, null]
+  ]
+};
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  grid = createEmpty2DArray(gridSize,gridSize);
 }
 
 function draw() {
   background(220);
+  displayGrid();
 }
 
 class Tetris {
-  constructor(x, y) {
+  constructor(x, y, type) {
     this.x = x;
     this.y = y;
-  }
-
-  blocks() {
-    
+    this.type = type;
   }
 }
 
+
+function createEmpty2DArray(rows,cols){
+  let grid = [];
+  for (let y=0; y<rows; y++){
+    grid.push([]);
+    for (let x=0; x<cols; x++){
+      grid[y].push(0);
+    }
+  }
+  return grid;
+}
+
+function displayGrid(){
+  let cellWidth = width/gridSize;
+  let cellHeight = height/gridSize;
+  
+  for (let y=0; y<gridSize; y++){
+    for (let x=0; x<gridSize; x++){
+      if (grid[y][x] === 0){
+        fill("white");
+      }
+      noStroke();
+      rect(x*cellWidth, y*cellHeight, cellWidth, cellHeight);
+    }
+  }
+}
